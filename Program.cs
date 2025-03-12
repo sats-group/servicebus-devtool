de28ef04d8c65c0a5fb7382dc89d05915bfcb669
Håvar Nøvik <havar.novik@sats.no>
2025-03-12 12:47:50 +0100

    Add functionality.

--- Program.cs
+++ Program.cs
@@ -1,17 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Azure.Identity;
 using Azure.Messaging.ServiceBus;
 using CommandLine;
 
-return await Parser.Default.ParseArguments<PeekOptions, object>(args)
+return await Parser.Default.ParseArguments<PeekOptions, AddOptions>(args)
     .MapResult(
         (PeekOptions opts) => HandlePeek(opts),
+        (AddOptions opts) => HandleAdd(opts),
         _ => Task.FromResult(1));
 
-static async Task<int> HandlePeek(PeekOptions options)
+static async Task<int> HandleAdd(AddOptions options)
 {
     var cts = new CancellationTokenSource();
     Console.CancelKeyPress += async (_, e) =>
@@ -26,13 +29,127 @@ static async Task<int> HandlePeek(PeekOptions options)
 
     try
     {
-        var azureCliCredentials = new AzureCliCredential();
+        var credential = new ChainedTokenCredential(
+            new AzureCliCredential(),
+            new InteractiveBrowserCredential(),
+            new VisualStudioCredential(),
+            new AzurePowerShellCredential());
+
+        var split = options.Destination.Split("/", count: 2);
+        if (split.Length != 2)
+        {
+            return 1;
+        }
+
+        var serviceBusClient = new ServiceBusClient(split[0], credential);
+        var sender = serviceBusClient.CreateSender(split[1]);
+
+
+        var messages = new Queue<ServiceBusMessage>();
+
+        await using var stdIn = Console.OpenStandardInput();
+        using var reader = new StreamReader(stdIn, Encoding.UTF8);
+        while (true)
+        {
+            var line = await reader.ReadLineAsync(cts.Token);
+            if (string.IsNullOrEmpty(line))
+            {
+                break;
+            }
+            var visibilityTimeout =
+                options.VisibilityTimeout is {} visibilityTimeoutOptions
+                    ? (TimeSpan?)TimeSpan.FromSeconds(messages.Count * visibilityTimeoutOptions)
+                    : null;
+
+            var msg = new ServiceBusMessage(BinaryData.FromString(line));
+            if (visibilityTimeout is not null)
+            {
+                msg.ScheduledEnqueueTime = DateTimeOffset.UtcNow.Add(visibilityTimeout.Value);
+            }
+
+            messages.Enqueue(msg);
+        }
+
+        var messageCount = messages.Count;
+
+        // Copied from: https://learn.microsoft.com/en-us/dotnet/api/overview/azure/messaging.servicebus-readme?view=azure-dotnet#sending-a-batch-of-messages
+        while (messages.Count > 0)
+        {
+            // start a new batch
+            using var messageBatch = await sender.CreateMessageBatchAsync(cts.Token);
+
+            // add the first message to the batch
+            if (messageBatch.TryAddMessage(messages.Peek()))
+            {
+                // dequeue the message from the .NET queue once the message is added to the batch
+                messages.Dequeue();
+            }
+            else
+            {
+                // if the first message can't fit, then it is too large for the batch
+                throw new Exception($"Message {messageCount - messages.Count} is too large and cannot be sent.");
+            }
+
+            // add as many messages as possible to the current batch
+            while (messages.Count > 0 && messageBatch.TryAddMessage(messages.Peek()))
+            {
+                // dequeue the message from the .NET queue as it has been added to the batch
+                messages.Dequeue();
+            }
+
+            // now, send the batch
+            if (options.DryRun)
+            {
+                Console.WriteLine($"Would've sent {messageBatch.Count} messages to {sender.FullyQualifiedNamespace}/{sender.EntityPath}.");
+            }
+            else
+            {
+                await sender.SendMessagesAsync(messageBatch, cts.Token);
+            }
+
+            // if there are any remaining messages in the .NET queue, the while loop repeats
+        }
+
+        return 0;
+    }
+    catch (Exception e)
+    {
+        await using var stdErr = Console.OpenStandardError();
+        await using var stdErrWriter = new StreamWriter(stdErr);
+        await stdErrWriter.WriteLineAsync(e.Message);
+        await stdErrWriter.WriteLineAsync(e.StackTrace);
+        return 1;
+    }
+}
+
+static async Task<int> HandlePeek(PeekOptions options)
+{
+    var cts = new CancellationTokenSource();
+    Console.CancelKeyPress += async (_, e) =>
+    {
+        await using var stdErr = Console.OpenStandardError();
+        await using var stdErrWriter = new StreamWriter(stdErr);
+        await stdErrWriter.WriteLineAsync("Stopping... (ctrl-c)");
+        await stdErrWriter.FlushAsync();
+        cts.Cancel();
+        e.Cancel = true;
+    };
+
+    // dotnet run -- add --dry-run 'sats-test-eun-servicebus-standard.servicebus.windows.net/activities'
+
+    try
+    {
+        var credential = new ChainedTokenCredential(
+            new AzureCliCredential(),
+            new InteractiveBrowserCredential(),
+            new VisualStudioCredential(),
+            new AzurePowerShellCredential());
 
         ServiceBusReceiver receiver;
         var split = options.Source.Split("/");
         if (split.Length == 3)
         {
-            var serviceBusClient = new ServiceBusClient(split[0], azureCliCredentials);
+            var serviceBusClient = new ServiceBusClient(split[0], credential);
             receiver = serviceBusClient.CreateReceiver(
                 split[1],
                 split[2],
@@ -48,7 +165,7 @@ static async Task<int> HandlePeek(PeekOptions options)
                 return 1;
             }
 
-            var serviceBusClient = new ServiceBusClient(split[0], azureCliCredentials);
+            var serviceBusClient = new ServiceBusClient(split[0], credential);
             receiver = serviceBusClient.CreateReceiver(
                 split[1],
                 $"{split[2]}/{split[3]}",
@@ -111,26 +228,28 @@ static async Task<int> HandlePeek(PeekOptions options)
     }
 }
 
+[Verb("add", HelpText = "Adds messages from stdin to a service bus queue/topic.")]
+class AddOptions
+{
+    [Value(0, MetaName = "destination", HelpText = "The name of the queue/topic to which the messages will be added.", Required = true)]
+    public required string Destination { get; init; }
+
+    [Option('t', "visibility-timeout-factor", HelpText = "This parameter decided the visibilityTimeout that is set to each messages passed from stdin, visibilityTimeout = visibility-timeout-factor * msgCount (in seconds).", Required = false)]
+    public double? VisibilityTimeout { get; init; }
+
+    [Option("dry-run", HelpText = "This parameter decides if the message will be added to the queue or just printed to stdout (default: false).", Required = false)]
+    public bool DryRun { get; init; }
+}
 
 [Verb("peek", HelpText = "Peek messages from a queue or topic subscription.")]
 class PeekOptions
 {
-    public PeekOptions(
-        string source,
-        int? numberOfMessages,
-        bool? delete)
-    {
-        Source = source;
-        Delete = delete;
-        NumberOfMessages = numberOfMessages != 0 ? numberOfMessages ?? 32 : 32;
-    }
-
     [Value(0, MetaName = "src", HelpText = "The name of the source queue from which the messages will be peeked from.", Required = true)]
-    public string Source { get; }
+    public required string Source { get; init; }
 
     [Option("messages", HelpText = "The number of messages to peek.", Required = false)]
-    public int NumberOfMessages { get; }
+    public int NumberOfMessages { get; init; } = 32;
 
     [Option("delete", HelpText = "If set, the messages are removed from the queue.", Required = false)]
-    public bool? Delete { get; }
+    public bool? Delete { get; init; }
 }
--- README.md
+++ README.md
@@ -1,6 +1,19 @@
+dotnet run -- --help
 ```
-dotnet run -- peek --help                                                                                                                                 master 938cb30
+Copyright (C) 2025 servicebus-devtool
 
+  peek       Peek messages from a queue or topic subscription.
+
+  add        Adds messages from stdin to a service bus queue/topic.
+
+  help       Display more information on a specific command.
+
+  version    Display version information.
+
+```
+
+dotnet run -- peek --help
+```
 servicebus-devtool 1.0.0
 Copyright (C) 2023 servicebus-devtool
 
@@ -13,4 +26,27 @@ Copyright (C) 2023 servicebus-devtool
   --version       Display version information.
 
   src (pos. 0)    Required. The name of the source queue from which the messages will be peeked from.
+```
+
+# Example usage
+
+`file.csv`:
+```csv
+157p220548
+222p179793
+161p201824
+802p53216
+147p271760
+156p277267
+253p127635
+801p179604
+161p207128
+157p220612
+152p250883
+801p182001
+```
+
+```sh
+awk '{ split($0,id,"p"); print "{\"bookingId\": \"" id[1] "book" id[2] "\", \"bookingState\": \"Planned\" }"; }' ./file.csv \
+  | dotnet run -- add -t 0.2 'namespace.servicebus.windows.net/queue'
 ```
